5124e157da59500c238a3d924c59ac96cb4a10fd
ravin-ship-it <xencomeon@gmail.com>
2026-05-30 08:37:21 +0530

    Fixed error for PC support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,24 +6,35 @@ return {
         priority = 1000,
         build = ":TSUpdate",
         config = function()
+            local utils = require("core.utils")
             local parser_install_dir = vim.fn.stdpath("data") .. "/site"
             
             -- Ensure the directory is in the runtimepath so Neovim can find the parsers
             vim.opt.runtimepath:prepend(parser_install_dir)
 
             -- The new main branch setup
+            -- Use git instead of curl (more stable on PC/Windows)
             require("nvim-treesitter").setup({
                 install_dir = parser_install_dir,
+                prefer_git = true,
+                auto_install = true,
             })
 
             -- Automatically install common parsers asynchronously
+            -- Note: 'vimdoc', 'vim', 'lua', 'c', and 'query' are bundled with Neovim 0.10+
             local parsers = { 
-                "bash", "c", "cpp", "lua", "vim", "vimdoc", "query", 
-                "rust", "go", "javascript", "typescript", "html", "css",
-                "scss", "json", "yaml", "markdown", "markdown_inline", "python"
+                "bash", "cpp", "rust", "go", "javascript", "typescript", 
+                "html", "css", "scss", "json", "yaml", "markdown", 
+                "markdown_inline", "python"
             }
             
-            -- Pcall so it doesn't crash on startup if offline
+            -- Only add bundled parsers if we have the tree-sitter CLI or if on Android
+            -- On PC, 'vimdoc' often requires the CLI to build, which causes ENOENT
+            if utils.is_android or vim.fn.executable("tree-sitter") == 1 then
+                vim.list_extend(parsers, { "c", "lua", "vim", "vimdoc", "query" })
+            end
+            
+            -- Pcall so it doesn't crash on startup if offline or if tree-sitter CLI is missing
             pcall(function()
                 require("nvim-treesitter").install(parsers)
             end)
